8b470181bc436684df49cf4f052eeb4337a21250
TomaSajt <tomasajtofficial@gmail.com>
2023-03-11 00:07:42 +0100

    First commit

---
+++ configuration.nix
@@ -0,0 +1,206 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    (import "${home-manager}/nixos")
+  ];
+
+  home-manager.users.toma = {
+    # Required version field
+    home.stateVersion = "22.11";
+    
+    home.username = "toma";
+    programs.bash.enable = true;
+
+    fonts.fontconfig.enable = true;
+
+    home.packages = with pkgs; [
+      
+      # User stuff
+      firefox      # Browser
+
+      # Utils
+      gh           # GitHub CLI
+      gparted      # Partition Management
+
+      # Support
+      ntfs3g       # NTFS Filesystem Support
+      libusb
+      ripgrep      # telescope.nvim support for grep
+
+      # Fonts
+      (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ];})
+
+      ### Languages ###
+      
+      # NodeJS
+      nodejs            
+      nodePackages.pnpm
+
+      # C#
+      dotnet-sdk
+
+      # Java
+      jdk
+    ];
+
+    programs.git = {
+      enable = true;
+      package = pkgs.gitFull;
+      userName = "Toma";
+      userEmail = "***REMOVED***";
+      extraConfig = {
+        github.user = "TomaSajt";
+        init.defaultBranch = "main";
+        credential.helper = "cache";
+        # credential.helper = "${pkgs.gitAndTools.gitFull}/bin/git-credential-libsecret";
+      };
+    };
+    programs.neovim = {
+      enable = true;
+      plugins = with pkgs.vimPlugins; [
+        yankring
+        vim-nix
+        plenary-nvim
+        telescope-nvim
+        telescope-fzf-native-nvim
+        telescope-file-browser-nvim
+        nvim-treesitter
+        nvim-web-devicons
+      ];
+    };
+  };
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = lib.optionalString (config.nix.package == pkgs.nixFlakes) "experimental-features = nix-command flakes";
+  };
+
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="3000", MODE="0666"
+  '';
+
+  # Not used currently
+  services.xserver.exportConfiguration = true;
+
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "toma-nixos-desktop"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Budapest";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "hu_HU.UTF-8";
+    LC_IDENTIFICATION = "hu_HU.UTF-8";
+    LC_MEASUREMENT = "hu_HU.UTF-8";
+    LC_MONETARY = "hu_HU.UTF-8";
+    LC_NAME = "hu_HU.UTF-8";
+    LC_NUMERIC = "hu_HU.UTF-8";
+    LC_PAPER = "hu_HU.UTF-8";
+    LC_TELEPHONE = "hu_HU.UTF-8";
+    LC_TIME = "hu_HU.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+ 
+  # Because some kind of race condition
+  systemd.services.display-manager.wants = [ "systemd-user-sessions.service" "multi-user.target" "network-online.target" ];
+  systemd.services.display-manager.after = [ "systemd-user-sessions.service" "multi-user.target" "network-online.target" ];
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "hu";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "hu101";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  users.users.toma = {
+    isNormalUser = true;
+    description = "Toma";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = [ ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+}
