579ec4e98e44e2d1e8aa84b1027d01d39ffb3f20
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-04-15 02:20:57 +0200

    Add alias

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -5,6 +5,7 @@
       snrs = "sudo nixos-rebuild switch";
       ll = "ls -la";
       code = "codium";
+      sdn = "shutdown now";
     };
     initExtra = ''
       export PS1="\[\033[1;32m\]\w \$ \[\033[0m\]"
