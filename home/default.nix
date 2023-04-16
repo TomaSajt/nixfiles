94647f6cee6826e37dff2538c4c9289c6fa2633c
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-04-16 22:48:24 +0200

    Bump

--- home/default.nix
+++ home/default.nix
@@ -35,6 +35,7 @@
       discord # Online Chat - UNSAFE
       obsidian # Note-taking - UNSAFE
       gimp # Image editing
+      unstable.ani-cli
 
       ### Utils ###
       gparted # Partition Management
