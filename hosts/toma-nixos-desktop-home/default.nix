f342daaf3c17c1597576dbfadba61722031da7f9
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-04-16 19:41:44 +0200

    Add lutris and nvidia driver

--- hosts/toma-nixos-desktop-home/default.nix
+++ hosts/toma-nixos-desktop-home/default.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -10,5 +10,21 @@
       download-dir = "/mnt/extra/transmission/download";
       incomplete-dir = "/mnt/extra/transmission/.incomplete";
     };
+  };
+
+
+  # Gets rid of some compatibility errors 
+  hardware.opengl.driSupport32Bit = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
+  home-manager.users.toma = {
+    home.packages = with pkgs; [
+      (lutris.override {
+        extraPkgs = pkgs: [
+          libdrm
+        ];
+      })
+    ];
   };
 }
