54e89b2e5ebca33405bc08b64cebd8ccdc12cc95
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-04-19 00:31:30 +0200

    Bump

--- home/xidlehook.nix
+++ home/xidlehook.nix
@@ -1,13 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, osConfig, ... }:
+let
+  i3lock-fancy = "${pkgs.i3lock-fancy}/bin/i3lock-fancy";
+  systemctl = "${osConfig.systemd.package}/bin/systemctl";
+in
 {
   services.xidlehook = {
     enable = true;
     not-when-audio = true;
-    not-when-fullscreen = true;
+    detect-sleep = true;
     timers = [
       {
         delay = 300;
-        command = "${pkgs.i3lock-fancy}/bin/i3lock-fancy";
+        command = "${i3lock-fancy}";
+      }
+      {
+        delay = 1500;
+        command = "${systemctl} suspend";
       }
     ];
   };
