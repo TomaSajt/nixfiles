58d51469d6426aebbfe0f011d334d4cb4aabe8c3
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-04-20 22:04:01 +0200

    Add catppuccino gtk themes

--- home/default.nix
+++ home/default.nix
@@ -31,45 +31,50 @@
   home = {
     username = "toma";
     stateVersion = "22.11";
-    packages = with pkgs; [
+    sessionVariables = {
+      DOTNET_CLI_TELEMETRY_OPTOUT = "true";
+    };
+    packages = with pkgs;
+      [
 
-      ### User stuff ###
-      discord # Online Chat - UNFREE
-      obsidian # Note-taking - UNFREE
-      gimp # Image editing
-      unstable.osu-lazer # Rythm moment - UNFREE
-      unstable.ani-cli # Anime moment
+        ### User stuff ###
+        discord # Online Chat - UNFREE
+        obsidian # Note-taking - UNFREE
+        gimp # Image editing
+        unstable.osu-lazer # Rythm moment - UNFREE
+        unstable.ani-cli # Anime moment
 
-      ### Utils ###
-      gparted # Partition Management
-      quark-goldleaf # Nintendo Switch File Transfer Client
-      zip # Zip compression utils
-      unzip
-      wget
-      file
-      qdirstat
+        ### Utils ###
+        lxappearance # Look at themes (just don't switch them)
+        gparted # Partition Management
+        quark-goldleaf # Nintendo Switch File Transfer Client
+        zip # Zip compression utils
+        unzip
+        wget
+        file
+        qdirstat
 
-      ### Support ###
-      ntfs3g # NTFS Filesystem Support
-      ripgrep # telescope.nvim support for grep
-      xclip # Clipboard support (for synced neovim clipboard)
-      fd # `find` alternative (used for neovim)
+        ### Support ###
+        ntfs3g # NTFS Filesystem Support
+        ripgrep # telescope.nvim support for grep
+        xclip # Clipboard support (for synced neovim clipboard)
+        fd # `find` alternative (used for neovim)
 
-      ### Languages ###
+        ### Languages ###
 
-      # C++
-      gcc
+        # C++
+        gcc
 
-      # NodeJS
-      nodejs
-      nodePackages.pnpm # npm alternative
+        # NodeJS
+        nodejs
+        nodePackages.pnpm # npm alternative
 
-      # Dotnet - C#/F#
-      unstable.dotnet-sdk
+        # Dotnet - C#/F#
+        unstable.dotnet-sdk
 
-      # Python
-      python311
+        # Python
+        python311
 
-    ];
+      ];
   };
 }
--- home/gtk.nix
+++ home/gtk.nix
@@ -1,6 +1,20 @@
 { pkgs, ... }: {
   gtk = {
     enable = true;
+    theme = {
+      # it says light, but it's actually dark
+      name = "Catppuccin-Latte-Compact-Blue-Light";
+      package = pkgs.unstable.catppuccin-gtk.override {
+        accents = [ "blue" ];
+        size = "compact";
+        tweaks = [ "rimless" /*"black"*/ ];
+        variant = "latte";
+      };
+    };
+    cursorTheme = {
+      name = "Catppuccin-Latte-Dark-Cursors";
+      package = pkgs.catppuccin-cursors.latteDark;
+    };
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
