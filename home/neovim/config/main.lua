5a64f9ab0b34ccd856a63ecea103e831dd18aa07
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-05-17 01:12:24 +0200

    Remove context start underlining

--- home/neovim/config/main.lua
+++ home/neovim/config/main.lua
@@ -51,5 +51,4 @@ require("nvim-autopairs").setup {}
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
-    show_current_context_start = true,
 }
