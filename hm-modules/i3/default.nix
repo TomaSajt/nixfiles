9c23eb1715508daec09694dfbd679fd49fba61ab
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-10-03 18:55:05 +0200

    Bump

--- hm-modules/i3/default.nix
+++ hm-modules/i3/default.nix
@@ -272,19 +272,10 @@ in
               "alsa_output.pci-0000_00_1f.3.analog-stereo" = "";
               "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp__sink" = "";
             };
-
-            click = [{
-              button = "left";
-              cmd = "${pavucontrol} --tab=3";
-            }];
           }
           {
             block = "net";
             format = " {$icon  $ssid|Wired connection} [$device] ";
-            click = [{
-              button = "left";
-              cmd = "${nm-connection-editor}";
-            }];
           }
         ]
         ++ lib.optional cfg.show-battery {
