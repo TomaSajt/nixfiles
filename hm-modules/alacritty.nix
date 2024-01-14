0efc0951f29324b079a4ed2b6b747b4e12946534
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2024-01-14 02:30:03 +0100

    Bump

--- hm-modules/alacritty.nix
+++ hm-modules/alacritty.nix
@@ -8,55 +8,58 @@ in
     enable = lib.mkEnableOption "alacritty";
     font-size = lib.mkOption {
       type = lib.types.number;
-      apply = toString;
       default = 10;
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    programs.alacritty = {
-      enable = true;
+  config = lib.mkIf cfg.enable
+    {
+      programs.alacritty = {
+        enable = true;
+      };
+      xdg.configFile."alacritty/alacritty.toml".source = pkgs.writers.writeTOML "alacritty.toml" {
+        colors = {
+          bright = {
+            black = "0x72696a";
+            blue = "0xf38d70";
+            cyan = "0x85dacc";
+            green = "0xadda78";
+            magenta = "0xa8a9eb";
+            red = "0xfd6883";
+            white = "0xfff1f3";
+            yellow = "0xf9cc6c";
+          };
+          normal = {
+            black = "0x2c2525";
+            blue = "0xf38d70";
+            cyan = "0x85dacc";
+            green = "0xadda78";
+            magenta = "0xa8a9eb";
+            red = "0xfd6883";
+            white = "0xfff1f3";
+            yellow = "0xf9cc6c";
+
+          };
+          primary = {
+            background = "0x2D2A2E";
+            foreground = "0xfff1f3";
+          };
+        };
+
+        font = {
+          builtin_box_drawing = true;
+          size = cfg.font-size;
+          normal = {
+            family = "monospace";
+          };
+        };
+        window = {
+          opacity = 0.8;
+          padding = {
+            x = 10;
+            y = 10;
+          };
+        };
+      };
     };
-
-    xdg.configFile."alacritty/alacritty.yml".text = ''
-      window:
-        opacity: 0.8
-        padding:
-          x: 10
-          y: 10
-      font:
-        normal:
-          family: monospace
-        size: ${cfg.font-size}
-        builtin_box_drawing: true
-
-      colors:
-        # Default colors
-        primary:
-          background: '0x2D2A2E'
-          foreground: '0xfff1f3'
-
-        # Normal colors
-        normal:
-          black:   '0x2c2525'
-          red:     '0xfd6883'
-          green:   '0xadda78'
-          yellow:  '0xf9cc6c'
-          blue:    '0xf38d70'
-          magenta: '0xa8a9eb'
-          cyan:    '0x85dacc'
-          white:   '0xfff1f3'
-
-        # Bright colors
-        bright:
-          black:   '0x72696a'
-          red:     '0xfd6883'
-          green:   '0xadda78'
-          yellow:  '0xf9cc6c'
-          blue:    '0xf38d70'
-          magenta: '0xa8a9eb'
-          cyan:    '0x85dacc'
-          white:   '0xfff1f3'
-    '';
-  };
 }
