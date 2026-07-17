44cfa0065b8bbc4ff96aa5f6cb4a4e7935935669
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-07-17 18:54:01 +0200

    bump

--- modules/dev/dotnet.nix
+++ modules/dev/dotnet.nix
@@ -12,8 +12,8 @@
       dotnet-sdks =
         with pkgs.dotnetCorePackages;
         combinePackages [
-          sdk_8_0
-          sdk_9_0
+          sdk_11_0
+          sdk_10_0
         ];
     in
     {
